03b744fe01adfa8767ae20e1736ed7cb3c8e79c7
yingxuanguo <yingxuan@usc.edu>
2020-04-08 01:28:09 -0700

    finished guardian+nytimes home+section api

---
+++ guardian/guardian.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+// useful Guardian consts
+const guardian_default_img_url = "https://assets.guim.co.uk/images/eada8aa27c12fe2d5afa3a89d3fbae0d/fallback-logo.png";
+const guardian_api_key = "8f86228c-9df5-451e-a505-f7979e6ec8a3";
+
+// get Guardian home articles
+function get_guardian_home() {
+  return axios.get(`https://content.guardianapis.com/search?api-key=${guardian_api_key}&section=(world|sport|business|technology|politics)&show-blocks=all`)
+    .then(response => {
+      console.log(`Getting Guardian home - status: ${response.status}`);
+      return response.data.response.results;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+// get Guardian articles by section
+function get_guardian_by_section(section) {
+  return axios.get(`https://content.guardianapis.com/${section}?api-key=${guardian_api_key}&show-blocks=all`)
+    .then( response => {
+      console.log(`Getting Guardian section \'${section}\' - status: ${response.status}`);
+      return response.data.response.results;
+    })
+    .catch( error => {
+      console.log(error);
+    });
+}
+
+// return processed Guardian results
+function process_guardian_results(data) {
+  const results = {articles: []};
+  for (let i = 0; i < data.length; i++) {
+    const article = {};
+    
+    // validate title
+    if (data[i].webTitle) article.title = data[i].webTitle;
+    else { console.log(`\tskipping article ${i}: title missing.`); continue; }
+    
+    // validate image url
+    let assets;
+    try { assets = data[i].blocks.main.elements[0].assets; }
+    catch (e) {}
+    finally {
+      if (assets && assets.length > 0) { article.image = assets[assets.length - 1].file; }
+      else article.image = guardian_default_img_url;
+    }
+    
+    // validate sectionId
+    if (data[i].sectionId) {
+      if (data[i].sectionId === 'sport') data[i].sectionId = 'sports';
+      article.sectionId = data[i].sectionId;
+    }
+    else { console.log(`\tskipping article ${i}: sectionId missing.`); continue; }
+    
+    // validate publication date
+    const date_patt = /\d{4}-\d{2}-\d{2}/;
+    if (data[i].webPublicationDate)
+      article.date = date_patt.exec(data[i].webPublicationDate)[0];
+    else { console.log(`\tskipping article ${i}: date missing.`); continue; }
+    
+    // validate description
+    if (data[i].blocks.body[0].bodyTextSummary) {
+      article.descp = data[i].blocks.body[0].bodyTextSummary;
+    }
+    else { console.log(`\tskipping article ${i}: description missing.`); continue; }
+    
+    // push validated article
+    results.articles.push(article);
+  }
+  return results;
+}
+
+router.get('/', (req, res) => {
+  get_guardian_home()
+    .then(data => {
+      res.json(process_guardian_results(data))
+    })
+})
+
+router.get('/:sectionId', (req, res) => {
+  const sections = ['world', 'politics', 'business', 'technology', 'sports'];
+  if (!sections.includes(req.params.sectionId)) {
+    res.json({error: {message: "Unknown section ID."}})
+  }
+  else {
+    const sectionId = (req.params.sectionId === 'sports') ? 'sport' : req.params.sectionId;
+    get_guardian_by_section(sectionId)
+      .then(data => {
+        res.json(process_guardian_results(data))
+      })
+  }
+})
+
+module.exports = router;
---
+++ nytimes/nytimes.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+// useful NYTimes consts
+const nytimes_default_img_url = "https://upload.wikimedia.org/wikipedia/commons/0/0e/Nytimes_hq.jpg";
+const nytimes_api_key = "1HzpS5vMG5GY8t5nTyN0Gj2SssDJ71Ye";
+
+// get NYTimes home articles
+function get_nytimes_home() {
+  return axios.get(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${nytimes_api_key}`)
+    .then(response => {
+      console.log(`Getting NYTimes home - status: ${response.status}`);
+      return response.data.results.filter(filter_nytimes_section).slice(0,10);
+    })
+    .catch( error => {
+      console.log(error);
+    });
+}
+
+// filter home articles to selected sections
+function filter_nytimes_section(article) {
+  const sections = ['world', 'business', 'technology', 'sports'];
+  return sections.includes(article.section) || article.subsection === 'politics';
+}
+
+// get NYTimes articles by section
+function get_nytimes_section(section) {
+  return axios.get(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${nytimes_api_key}`)
+    .then( response => {
+      console.log(`Getting NYTimes section \'${section}\' - status: ${response.status}`);
+      return response.data.results.slice(0,10);
+    })
+    .catch( error => {
+      console.log(error);
+    });
+}
+
+// return processed NYTimes results
+function process_nytimes_results(data) {
+  const results = {articles: []};
+  for (let i = 0; i < data.length; i++) {
+    const article = {};
+    
+    // validate title
+    if (data[i].title) article.title = data[i].title;
+    else { console.log(`\tskipping article ${i}: title missing.`); continue; }
+    
+    // validate img url
+    try {
+      if (data[i].multimedia && data[i].multimedia.length > 0) {
+        const image = data[i].multimedia.filter((multimedia) => multimedia.width > 2000);
+        article.image = image[0].url;
+      }
+    }
+    catch (e) {}
+    finally { if (!article.image) article.image = nytimes_default_img_url; }
+  
+    // validate sectionId
+    if (data[i].section) {
+      // NYTimes returns politics articles with section='[country]' and subsection='politics'
+      if (data[i].subsection === 'politics') article.sectionId = data[i].subsection;
+      else article.sectionId = data[i].section;
+    }
+    else { console.log(`\tskipping article ${i}: sectionId missing.`); continue; }
+    
+      // validate publication date
+    const date_patt = /\d{4}-\d{2}-\d{2}/;
+    if (data[i].published_date)
+      article.date = date_patt.exec(data[i].published_date)[0];
+    else { console.log(`\tskipping article ${i}: date missing.`); continue; }
+  
+    // validate description
+    if (data[i].abstract) article.descp = data[i].abstract;
+    else { console.log(`\tskipping article ${i}: description missing.`); continue; }
+  
+    // push validated article
+    results.articles.push(article);
+  }
+  return results;
+}
+
+router.get('/', (req, res) => {
+  get_nytimes_home()
+    .then(data => {
+      res.json(process_nytimes_results(data))
+    })
+})
+
+router.get('/:sectionId', (req, res) => {
+  const sections = ['world', 'politics', 'business', 'technology', 'sports'];
+  if (!sections.includes(req.params.sectionId)) {
+    res.json({error: {message: "Unknown section ID."}})
+  }
+  else {
+    get_nytimes_section(req.params.sectionId)
+      .then(data => {
+        res.json(process_nytimes_results(data))
+      })
+  }
+})
+
+module.exports = router;
